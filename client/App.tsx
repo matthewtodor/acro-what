e640089073ec1efcb04458a0bd6c91e0803c8131
Matthew Todor <unclejunglegeorge@gmail.com>
2023-02-09 15:36:15 -0700

    added firebase baseline info

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./pages/home/home";
 import Profile from "./pages/profile/profile";
 import Login from "./pages/login/login";
 import Register from "./pages/register/register";
-
+import { collection, addDoc } from "firebase/firestore";
 type RootStackParamList = {
 	Home: { name: string };
 	Login: { name: string };
@@ -13,8 +12,12 @@ type RootStackParamList = {
 	Profile: { name: string };
 };
 const Stack = createNativeStackNavigator<RootStackParamList>();
-
 export default function App(): JSX.Element {
+	// try {
+	// 	const docRef = await addDoc(collection(db, "users"), )
+	// } catch {
+
+	// }
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
---
+++ client/firebase.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyC9-KSJMCClNSiER7S_GeUb_RdQMzez8Jw",
+	authDomain: "acro-what.firebaseapp.com",
+	projectId: "acro-what",
+	storageBucket: "acro-what.appspot.com",
+	messagingSenderId: "841843441980",
+	appId: "1:841843441980:web:1c3282dfb6add7960c1e39",
+	measurementId: "G-C8K11WWWRJ",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// const db = firebase.firestore(app)
